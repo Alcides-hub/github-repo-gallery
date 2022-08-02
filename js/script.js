96ebfba67ad0310c2b39ca5e36cbca5fcd1ac34e
alcides_t <alcides.dml@icloud.com>
2022-08-02 10:37:05 +0900

    Verified the code and checked the webpage and debugged

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 // this is where your profile will appear.
 const overview = document.querySelector(".overview");
-const username = "Alcides-hub";
+const username = "Alcides-hub"; // this global variable sets the username of Github profile.
 
-const gitUserInfo = async function () {
-    const userInfo = await fetch(`https://api.github.com/users/${username}`);
-    const data = await userInfo.json();
-    displayUserInfo(data);
+const gitProfileInfo = async function () {  //this function will fetch data from github API by adding the user end point 
+    const userInfo = await fetch (`https://api.github.com/users/${username}`); //fetches the API
+    const data = await userInfo.json(); // resolves the JSON response
+    displayUserInfo(data); //at the async function to fetch my github data, call the function displaying the user information and pass Json data as argument.
 };
 
-gitUserInfo();
+gitProfileInfo(); // call the function
 
-const displayUserInfo = function (data) {
-    const div = document.createElement("div");
-    div.classList.add("user-info");
-    div.innerHTML = `
+const displayUserInfo = function (data) { //this function will pull information from my profile and display those data in the webpage.
+    const div = document.createElement("div"); //this creates a new div element using createelement.
+    div.classList.add("user-info"); // this adds a class called user-info in your HTML page 
+    div.innerHTML = ` 
     <figure>
       <img alt="user avatar" src=${data.avatar_url} />
     </figure>
@@ -21,7 +21,7 @@ const displayUserInfo = function (data) {
       <p><strong>Name:</strong> ${data.name}</p>
       <p><strong>Bio:</strong> ${data.bio}</p>
       <p><strong>Location:</strong> ${data.location}</p>
-      <p><strong>Number of public repos:</strong> ${data.prublic_repos}</p>
-    </div>`;
-    overview.append(div);
+      <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`; // this populates the div with elements for figure, image, and paragraphs and you will input Json data in the placeholders.
+    overview.append(div); //this appends the div to the overview class.
 };
